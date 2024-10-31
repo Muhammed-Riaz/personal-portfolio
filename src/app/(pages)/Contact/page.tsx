51131702534bc1d897bd10d3f9e6ca203b253ea7
Riaz zaur <riazahmedzaur110@gmail.com>
2024-10-31 11:15:27 +0500

    my personal portfolio

---
+++ src/app/(pages)/Contact/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+import "../styles/contact.css"
+import {  motion } from "framer-motion";
+function Contact() {
+  return (
+    <motion.div
+    initial={{ opacity: 0.0, y: 40 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{
+      delay: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+    }}>
+      
+    <main className="main">
+          <div className="headin"><h1 className="h111">Contact Us</h1></div>
+    <div className="container11">
+      <div className="card">
+        <label htmlFor="Name" className="label">Full name:</label>
+        <br />
+        <input className="input" type="text" required placeholder="Enter Your Name"/><br />
+        <label className="label" htmlFor="Number">Phone:</label>
+        <br />
+        <input className="input" type="tel" required placeholder="Enter your Number"/><br />
+        <label className="label" htmlFor="Email">Email:</label>
+        <br />
+        <input className="input" type="email" required placeholder="example@gmail.com" />
+        <br />
+        <label className="label" htmlFor="message">Message:</label>
+        <br />
+        <textarea className="textarea" name="message" required placeholder="message ..."></textarea><br />
+        <button type="submit" className="button">Submit</button>
+      </div>
+
+
+    </div>
+  </main>
+
+  </motion.div>
+  )
+}
+
+export default Contact
